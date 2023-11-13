fa4316d105604400b60b37c1db38eeb16e775c57
Wolfie <32025746+Wolfkid200444@users.noreply.github.com>
2023-11-13 00:34:05 -0400

    feat: Added all patches needed

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,50 @@
-import { Injector, Logger, webpack } from "replugged";
+import { Injector, Logger, common, webpack } from "replugged";
 
+const { users } = common;
 const inject = new Injector();
 const logger = Logger.plugin("UerPFP");
 
-export async function start(): Promise<void> {
+let cache = {
+  avatars: {},
+  badges: {},
+};
 
+const getAvatar = webpack.getByProps("getUserAvatarSource");
+
+export async function start(): Promise<void> {
+  const data = await fetch(`https://userpfp.github.io/UserPFP/source/data.json`);
+  if (data.ok) cache = await data.json();
+
+  inject.after(users, "getUser", (args, res) => {
+    const profiles = cache.avatars[res?.id];
+
+    const avatar = res?.avatar ?? "0";
+    if (profiles) {
+      res.avatar = profiles ?? avatar;
+    }
+  });
+
+  inject.after(getAvatar, "getUserAvatarURL", ([{ id }], res) => {    
+    addAvatar(id);
+  });
+
+  inject.after(getAvatar, "getUserAvatarSource", ([{ id }], res) => {
+    const custom = addAvatar(id);
+    if (!custom) return;
+
+    return custom ? { uri : custom } : res;
+  });
 }
 
 export function stop(): void {
   inject.uninjectAll();
+}
+
+function addAvatar(id: string) {
+  if (!cache.avatars[id]) return;
+
+  const avatars = cache.avatars[id];
+
+  const url = new URL(avatars);
+  return url.toString();
 }
